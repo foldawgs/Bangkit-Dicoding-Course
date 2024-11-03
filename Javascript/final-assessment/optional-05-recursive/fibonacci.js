835091453e8ad01c6f97f0a0e7a6f5f88fb134e2
foldawgs <wahyu.isnantia08@gmail.com>
2024-11-03 15:38:51 +0700

    update assesment 5

--- Javascript/final-assessment/optional-05-recursive/fibonacci.js
+++ Javascript/final-assessment/optional-05-recursive/fibonacci.js
@@ -1,5 +1,13 @@
 function fibonacci(n) {
-
+    if (n === 0) {
+        return [0];
+    } else if (n === 1) {
+        return [0, 1]; 
+    } else {
+        const series = fibonacci(n - 1);
+        series.push(series[series.length - 1] + series[series.length - 2]);
+        return series;
+    }
 }
 
 // Jangan hapus kode di bawah ini!
